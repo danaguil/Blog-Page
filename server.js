7e99d74b206e0eb803e34b790e4e5d6d4358140e
danaguil <danaguilarfabian@gmail.com>
2025-09-05 13:49:27 -0700

    Fixed server.js so run on local host

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
+// server.js
 const express = require('express');
 const path = require('path');
 
+// Create an Express application
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Serve everything inside /public
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); // servign static files from 'public' directory
 
+// serve login.html at "/"
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
