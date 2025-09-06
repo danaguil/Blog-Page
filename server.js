cfb0d1e8621ae744f65c4ca76a4b7b5d12902997
danaguil <danaguilarfabian@gmail.com>
2025-09-06 15:55:24 -0700

    Add article action buttons and update login status API

--- server.js
+++ server.js
@@ -17,6 +17,15 @@ app.get('home', (_req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
+// telling express if someone is logged in
+app.get('/api/isLoggedIn', (_req, res) => {
+  if(req.session && req.session.user) {
+    res.json({ user: true }); // logged in
+  } else {
+    res.json({ user: null }); // not logged in
+  }
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
